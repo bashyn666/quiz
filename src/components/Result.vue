540d3fa931f3ab59b9e38c7dd22fde5ba479168f
bashyn666 <bashyn666@gmail.com>
2021-06-28 01:41:40 +0300

    Init

---
+++ src/components/Result.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="ctr">
+    <div class="result">
+        <div class="title">You got sample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
+    </div>
+    <button type="button" class="reset-btn">Reset</button>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
